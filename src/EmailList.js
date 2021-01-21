567b99d3c405d778d025811cb4b77ecf9434128d
trieule47 <62042242+trieule47@users.noreply.github.com>
2021-01-21 22:36:09 +0700

    up header , sidebar

---
+++ src/EmailList.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Checkbox, IconButton } from "@material-ui/core";
+import "./EmailList.css";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from '@material-ui/icons/Redo';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+function EmailList() {
+    return (
+        <div className="emailList">
+            <div className="emailList__setting">
+                <div className="emailList__settingLeft">
+                    <Checkbox />
+
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+                <div className="emailList__settingsRight">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EmailList
---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import "./Header.css";
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { IconButton, Avatar } from "@material-ui/core";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png" alt="gmail"/>
+            </div>
+            <div className="header__middle">
+                <SearchIcon/>
+                <input placeholder="Search email" type="text" />
+                <ArrowDropDownIcon className="header__inputCaret"/>
+            </div>
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
